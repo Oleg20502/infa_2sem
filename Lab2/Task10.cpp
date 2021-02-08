45bf60ca0a09071da650680c7f70892b86448ea5
Oleg20502 <oleg20502@gmail.com>
2021-02-08 21:10:38 +0300

    Added Task 10

--- Lab2/Task10.cpp
+++ Lab2/Task10.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
+
+string make_str(string a, int b)
+{
+    string str = "";
+    for (int i = 0; i < b; i++){
+        str += a;
+    }
+    return str;
+}
+
+string vnum(int a)
+{
+    return make_str("<", a / 10) + make_str("v", a%10);
+}
+
+string encode(int n)
+{
+    if (n < 60){
+        return vnum(n);
+    }
+    else{
+        return encode(n/60) + "." + vnum(n%60);
+    }
+}
 
 int main()
 {
-    int N, n;
+    int N;
     cin >> N;
-
+    cout << encode(N);
     return 0;
 }
